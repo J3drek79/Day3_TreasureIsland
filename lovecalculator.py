9712f7855d77b3b5757891dfec7744f269571d29
J3drek79 <117525013+J3drek79@users.noreply.github.com>
2022-11-07 20:14:47 +0100

    Update lovecalculator.py

--- lovecalculator.py
+++ lovecalculator.py
@@ -19,7 +19,7 @@ count_e1 = namecombined.count("e")
 count1 = count_t + count_r + count_u + count_e
 count2 = count_l + count_o + count_v + count_e1
 countcombined = int((f"{count1}{count2}"))
-if countcombined < 10 and countcombined > 90:
+if countcombined < 10 or countcombined > 90:
     print(f"Your score is {countcombined}, you go together like coke and mentos.")
 elif countcombined >= 40 and countcombined <= 50:
     print(f"Your score is {countcombined}, you are alright together.")
